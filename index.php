1553ec1a41e0f1e74c1f5cbf9e3076cdfb8af5bc
UTTAM PANDEY <davuttam@gmail.com>
2016-06-01 17:55:52 +0530

    json implemented in login db

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   <?php include_once('header.php'); ?>
   <body>
     <?php include('login.php'); ?>
-    <script src="js/lib/jquery-2.1.4.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+     <script src="js/lib/jquery-2.1.4.min.js"></script>
     <script src="js/lib/jquery-ui.js"></script>
     <script src='assets/js/jquery.nicescroll.min.js'></script>
     <script src='assets/js/wow.min.js'></script>
